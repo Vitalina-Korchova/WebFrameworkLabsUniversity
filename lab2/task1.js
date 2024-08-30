3ebbde12fb6929cd66d0bf68ad87b524906b40f0
VitalinaKorchova <vitalina.korchova@gmail.com>
2024-08-30 15:25:27 +0300

    fixed task2

--- lab2/task1.js
+++ lab2/task1.js
@@ -1,66 +1,65 @@
-var Cat = /** @class */ (function () {
-    function Cat(name, species) {
+"use strict";
+class Cat {
+    constructor(name, species) {
         this.name = name;
         this.species = species;
     }
-    Cat.prototype.move = function () {
-        console.log("".concat(this.name, " \u0445\u043E\u0434\u0438\u0442\u044C \u043D\u0430 \u0447\u043E\u0442\u0438\u0440\u044C\u043E\u0445 \u043B\u0430\u043F\u043A\u0430\u0445!"));
-    };
-    Cat.prototype.makeSound = function () {
-        console.log("".concat(this.species, ", \u0446\u0435\u0439 \u0432\u0438\u0434 \u0442\u0432\u0430\u0440\u0438\u043D \u0441\u0442\u0432\u043E\u0440\u044E\u0454 \u0442\u0430\u043A\u0438\u0439 \u0437\u0432\u0443\u043A \"\u041C\u044F\u0443-\u043C\u044F\u0443\"!"));
-    };
-    Cat.prototype.averageYearsLives = function () {
+    move() {
+        console.log(`${this.name} ходить на чотирьох лапках!`);
+    }
+    makeSound() {
+        console.log(`${this.species}, цей вид тварин створює такий звук "Мяу-мяу"!`);
+    }
+    averageYearsLives() {
         return 15;
-    };
-    return Cat;
-}());
-var Bird = /** @class */ (function () {
-    function Bird(name, species) {
+    }
+}
+class Bird {
+    constructor(name, species) {
         this.name = name;
         this.species = species;
     }
-    Bird.prototype.move = function () {
-        console.log("".concat(this.name, " \u043B\u0456\u0442\u0430\u0454 \u0437\u0430 \u0434\u043E\u043F\u043E\u043C\u043E\u0433\u043E\u044E \u043A\u0440\u0438\u043B!"));
-    };
-    Bird.prototype.makeSound = function () {
-        console.log("".concat(this.species, ", \u0446\u0435\u0439 \u0432\u0438\u0434 \u0442\u0432\u0430\u0440\u0438\u043D \u0441\u0442\u0432\u043E\u0440\u044E\u0454 \u0442\u0430\u043A\u0438\u0439 \u0437\u0432\u0443\u043A \"\u0422\u0440\u0456\u043A-\u0442\u0440\u0456\u043A\"!"));
-    };
-    Bird.prototype.averageYearsLives = function () {
+    move() {
+        console.log(`${this.name} літає за допомогою крил!`);
+    }
+    makeSound() {
+        console.log(`${this.species}, цей вид тварин створює такий звук "Трік-трік"!`);
+    }
+    averageYearsLives() {
         return 60;
-    };
-    return Bird;
-}());
-var Fish = /** @class */ (function () {
-    function Fish(name, species) {
+    }
+}
+class Fish {
+    constructor(name, species) {
         this.name = name;
         this.species = species;
     }
-    Fish.prototype.move = function () {
-        console.log("".concat(this.name, " \u043F\u043B\u0430\u0432\u0430\u0454 \u0443 \u0432\u043E\u0434\u0456!"));
-    };
-    Fish.prototype.makeSound = function () {
-        console.log("".concat(this.species, ", \u0446\u0435\u0439 \u0432\u0438\u0434 \u0442\u0432\u0430\u0440\u0438\u043D \u0441\u0442\u0432\u043E\u0440\u044E\u0454 \u0442\u0430\u043A\u0438\u0439 \u0437\u0432\u0443\u043A \"\u0411\u0443\u043B\u044C-\u0431\u0443\u043B\u044C\"!"));
-    };
-    Fish.prototype.averageYearsLives = function () {
+    move() {
+        console.log(`${this.name} плаває у воді!`);
+    }
+    makeSound() {
+        console.log(`${this.species}, цей вид тварин створює такий звук "Буль-буль"!`);
+    }
+    averageYearsLives() {
         return 20;
-    };
-    return Fish;
-}());
-var myCat = new Cat("Люцик", "Шотландский вислоухий кіт");
+    }
+}
+const myCat = new Cat("Люцик", "Шотландский вислоухий кіт");
 myCat.move();
 myCat.makeSound();
-var yearsC = myCat.averageYearsLives();
-console.log("\u0421\u0435\u0440\u0435\u0434\u043D\u044F \u0442\u0440\u0438\u0432\u0430\u043B\u0456\u0441\u0442\u044C \u0436\u0438\u0442\u0442\u044F ".concat(yearsC, " \u0440\u043E\u043A\u0456\u0432"));
+let yearsC = myCat.averageYearsLives();
+console.log(`Середня тривалість життя ${yearsC} років`);
 console.log("");
-var myBird = new Bird("Пепе", "Папуга звичайна");
+const myBird = new Bird("Пепе", "Папуга звичайна");
 myBird.move();
 myBird.makeSound();
-var yearsB = myBird.averageYearsLives();
-console.log("\u0421\u0435\u0440\u0435\u0434\u043D\u044F \u0442\u0440\u0438\u0432\u0430\u043B\u0456\u0441\u0442\u044C \u0436\u0438\u0442\u0442\u044F ".concat(yearsB, " \u0440\u043E\u043A\u0456\u0432"));
+let yearsB = myBird.averageYearsLives();
+console.log(`Середня тривалість життя ${yearsB} років`);
 console.log("");
-var myFish = new Fish("Тарас", "Окунь звичайни");
+const myFish = new Fish("Тарас", "Окунь звичайни");
 myFish.move();
 myFish.makeSound();
-var yearsF = myFish.averageYearsLives();
-console.log("\u0421\u0435\u0440\u0435\u0434\u043D\u044F \u0442\u0440\u0438\u0432\u0430\u043B\u0456\u0441\u0442\u044C \u0436\u0438\u0442\u0442\u044F ".concat(yearsF, " \u0440\u043E\u043A\u0456\u0432"));
+let yearsF = myFish.averageYearsLives();
+console.log(`Середня тривалість життя ${yearsF} років`);
 console.log("");
+//# sourceMappingURL=task1.js.map
--- lab2/task2.js
+++ lab2/task2.js
@@ -1,77 +1,76 @@
-var Circle = /** @class */ (function () {
-    function Circle(radius) {
+"use strict";
+class Circle {
+    constructor(radius) {
         this.radius = radius;
     }
-    Circle.prototype.getArea = function () {
+    getArea() {
         return Math.PI * this.radius * this.radius;
-    };
-    Circle.prototype.getPerimeter = function () {
+    }
+    getPerimeter() {
         return 2 * Math.PI * this.radius;
-    };
-    Circle.prototype.scale = function (factor) {
+    }
+    scale(factor) {
         this.radius *= factor;
-    };
-    return Circle;
-}());
-var Rectangle = /** @class */ (function () {
-    function Rectangle(width, height) {
+    }
+}
+class Rectangle {
+    constructor(width, height) {
         this.width = width;
         this.height = height;
     }
-    Rectangle.prototype.getArea = function () {
+    getArea() {
         return this.width * this.height;
-    };
-    Rectangle.prototype.getPerimeter = function () {
+    }
+    getPerimeter() {
         return 2 * (this.width + this.height);
-    };
-    Rectangle.prototype.scale = function (factor) {
+    }
+    scale(factor) {
         this.width *= factor;
         this.height *= factor;
-    };
-    return Rectangle;
-}());
-var Triangle = /** @class */ (function () {
-    function Triangle(sideA, sideB, sideC) {
+    }
+}
+class Triangle {
+    constructor(sideA, sideB, sideC) {
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
     }
-    Triangle.prototype.getArea = function () {
-        var s = (this.sideA + this.sideB + this.sideC) / 2;
+    getArea() {
+        const s = (this.sideA + this.sideB + this.sideC) / 2;
         return Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
-    };
-    Triangle.prototype.getPerimeter = function () {
+    }
+    getPerimeter() {
         return this.sideA + this.sideB + this.sideC;
-    };
-    Triangle.prototype.scale = function (factor) {
+    }
+    scale(factor) {
         this.sideA *= factor;
         this.sideB *= factor;
         this.sideC *= factor;
-    };
-    return Triangle;
-}());
-var shapes = [
+    }
+}
+const shapes = [
     new Circle(8),
     new Rectangle(5, 8),
     new Triangle(4, 7, 9)
 ];
-var totalArea = 0;
-var totalPerimetr = 0;
-shapes.forEach(function (shape) {
+let totalArea = 0;
+let totalPerimetr = 0;
+shapes.forEach(shape => {
     totalArea += shape.getArea();
     totalPerimetr += shape.getPerimeter();
 });
-console.log("\u0417\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u043F\u043B\u043E\u0449\u0430 \u0432\u0441\u0456\u0445 \u0444\u0456\u0433\u0443\u0440: ".concat(totalArea));
-console.log("\u0417\u0430\u0433\u0430\u043B\u044C\u043D\u0438\u0439 \u043F\u0435\u0440\u0438\u043C\u0435\u0442\u0440 \u0432\u0441\u0456\u0445 \u0444\u0456\u0433\u0443\u0440: ".concat(totalPerimetr));
+console.log(`Загальна площа всіх фігур: ${totalArea}`);
+console.log(`Загальний периметр всіх фігур: ${totalPerimetr}`);
 console.log("");
 console.log("З кофіцієнтом масштабу:");
 console.log("");
-shapes.forEach(function (shape) { return shape.scale(2); });
-var totalAreaNew = 0;
-var totalPerimetrNew = 0;
-shapes.forEach(function (shape) {
+shapes.forEach(shape => shape.scale(2));
+let totalAreaNew = 0;
+let totalPerimetrNew = 0;
+shapes.forEach(shape => {
     totalAreaNew += shape.getArea();
     totalPerimetrNew += shape.getPerimeter();
 });
-console.log("\u0417\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u043F\u043B\u043E\u0449\u0430 \u0432\u0441\u0456\u0445 \u0444\u0456\u0433\u0443\u0440 \u0437 \u043A\u043E\u0435\u0444\u0456\u0446\u0456\u0454\u043D\u0442\u043E\u043C: ".concat(totalAreaNew));
-console.log("\u0417\u0430\u0433\u0430\u043B\u044C\u043D\u0438\u0439 \u043F\u0435\u0440\u0438\u043C\u0435\u0442\u0440 \u0432\u0441\u0456\u0445 \u0444\u0456\u0433\u0443\u0440 \u0437 \u043A\u043E\u0435\u0444\u0456\u0446\u0456\u0454\u043D\u0442\u043E\u043C: ".concat(totalPerimetrNew));
+console.log(`Загальна площа всіх фігур з коефіцієнтом: ${totalAreaNew}`);
+console.log(`Загальний периметр всіх фігур з коефіцієнтом: ${totalPerimetrNew}`);
+//# sourceMappingURL=task2.js.map
