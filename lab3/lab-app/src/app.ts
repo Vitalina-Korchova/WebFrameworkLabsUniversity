87466c2bb8896cb3697f0c9b61260413bc22e305
VitalinaKorchova <vitalina.korchova@gmail.com>
2024-09-12 22:18:47 +0300

    validation basic

--- lab3/lab-app/src/app.ts
+++ lab3/lab-app/src/app.ts
@@ -2,6 +2,7 @@
 import {Book, User} from './models';
 import {LibraryService} from './services';
 import { Storage } from './storage';
+import { ValidationBook } from './validation';
 // etc.
 
 class App {
@@ -45,37 +46,42 @@ class App {
              Storage.clearData('book_items');
         });
 
-        document.querySelector('.input_add_book')?.addEventListener('click', ()=>{
+        document.querySelector('.input_add_book')?.addEventListener('click', (event)=>{
             //console.log('Add book button clicked');
+            event.preventDefault(); 
             const containerBooks = document.querySelector('.book_items');
             const nameBook = (document.querySelector('.input_name_book') as HTMLInputElement)?.value;    //перевторення на строки
             const authorBook = (document.querySelector('.input_author_book') as HTMLInputElement)?.value;
             const releaseYear = (document.querySelector('.input_release_year') as HTMLInputElement)?.value;
 
+            if(ValidationBook()){
+                const book = new Book(nameBook,authorBook, parseInt(releaseYear) );
+                this.libraryService.addBook(book);
+                //створення елемента
+                const bookItem = document.createElement('div');
+                const innerItem = document.createElement('div');
+                innerItem.classList.add('d-flex', 'justify-content-between');
+                const textElement = document.createElement('span');
+                textElement.textContent = book.printInfo();        //функція виведення інфи
+                textElement.classList.add('fs-5');
+                const buttonStatus = document.createElement('button');
+                buttonStatus.textContent = "Позичити";
+                buttonStatus.type = 'button';
+                buttonStatus.classList.add('btnBorrowOff','btn', 'btn-primary', 'btn-sm');
+                const line = document.createElement('hr');
+                innerItem.appendChild(textElement);
+                innerItem.appendChild(buttonStatus);
+                bookItem.appendChild(innerItem);
+                bookItem.appendChild(line);
+                containerBooks?.appendChild(bookItem);
 
-            const book = new Book(nameBook,authorBook, parseInt(releaseYear) );
-            this.libraryService.addBook(book);
-           
-            //створення елемента
-            const bookItem = document.createElement('div');
-            const innerItem = document.createElement('div');
-            innerItem.classList.add('d-flex', 'justify-content-between');
-            const textElement = document.createElement('span');
-            textElement.textContent = book.printInfo();        //функція виведення інфи
-            textElement.classList.add('fs-5');
-            const buttonStatus = document.createElement('button');
-            buttonStatus.textContent = "Позичити";
-            const line = document.createElement('hr');
-            innerItem.appendChild(textElement);
-            innerItem.appendChild(buttonStatus);
-            bookItem.appendChild(innerItem);
-            bookItem.appendChild(line);
-            containerBooks?.appendChild(bookItem);
 
-
-             // збереження HTML елементів у localStorage
-             Storage.saveElementState('book_items', containerBooks?.innerHTML || '');
-            }    
+                // збереження HTML елементів у localStorage
+                Storage.saveElementState('book_items', containerBooks?.innerHTML || '');
+            }
+            
+            
+        }    
         );
     }
 
@@ -109,11 +115,8 @@ class App {
             const textElement = document.createElement('span');
             textElement.textContent = user.printInfo();        //функція виведення інфи
             textElement.classList.add('fs-5');
-            const buttonStatus = document.createElement('button');
-            buttonStatus.textContent = "Позичити";
             const line = document.createElement('hr');
             innerUser.appendChild(textElement);
-            innerUser.appendChild(buttonStatus);
             userItem.appendChild(innerUser);
             userItem.appendChild(line);
             containerUsers?.appendChild(userItem);
--- lab3/lab-app/src/validation.ts
+++ lab3/lab-app/src/validation.ts
@@ -1,3 +1,14 @@
-export class Validation {
+export function ValidationBook(): boolean {
+    const formElements = document.querySelectorAll('.needs-validation');
+    let isValid = true;
+
+    Array.prototype.slice.call(formElements).forEach(function (formElem) {
+        if (!formElem.checkValidity()) {
+            isValid = false;
+        }
     
+        formElem.classList.add('was-validated'); 
+    });
+
+    return isValid;
 }
